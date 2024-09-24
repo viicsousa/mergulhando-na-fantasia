86d91c60d6488b52b3f198c2f8efbce7124ad265
viicsousa <victoria.vasconcelos@hotmail.com>
2024-09-24 10:34:56 -0300

    style: :zap: adjusted styled

--- src/app/components/navBar.tsx
+++ src/app/components/navBar.tsx
@@ -6,12 +6,12 @@ export default function NavBar() {
     <nav className="p-4 px-8">
     <div className="container mx-auto flex justify-between items-center">
       <div>
-        <a href="/"><Image src={Logo} alt="Logo do site"/></a>
+        <a href="/"><Image src={Logo} alt="Logo do site" className="w-12 cursor-pointer transform hover:scale-110 transition-transform duration-300"/></a>
       </div>
-      <ul className="flex space-x-6 text-custom-20 font-bold">
-        <li className="cursor-pointer hover:opacity-50">Sobre</li>
-        <li className="cursor-pointer hover:opacity-50">Resenha</li>
-        <li className="cursor-pointer hover:opacity-50">Contato</li>
+      <ul className="flex space-x-6 text-lg font-bold">
+        <li className="cursor-pointer transform hover:scale-110 transition-transform duration-300">Sobre</li>
+        <li className="cursor-pointer transform hover:scale-110 transition-transform duration-300">Resenha</li>
+        <li className="cursor-pointer transform hover:scale-110 transition-transform duration-300">Contato</li>
       </ul>
     </div>
   </nav>
