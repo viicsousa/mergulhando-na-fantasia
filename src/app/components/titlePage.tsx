27799d531aebaf99f8b7d4a7800d942932316095
viicsousa <victoria.vasconcelos@hotmail.com>
2024-10-04 10:55:21 -0300

    feat:

--- src/app/components/titlePage.tsx
+++ src/app/components/titlePage.tsx
@@ -1,15 +1,11 @@
 export default function TitlePage () {
   return (
-    <div>
-    <div className="container flex flex-col justify-center items-center col-auto gap-4">
-        <div className="w-2/4 h-[1px] bg-black mt-2">
-        </div>
-        <h1 className="text-5xl font-cinzel font-bold">
-          MERGULHANDO NA FANTASIA
-        </h1>
-        <div className="w-2/4 h-[1px] bg-black mt-2">
-        </div>
-      </div>
+    <div className=" flex flex-col justify-center items-center gap-4 p-6">
+      <div className="w-2/4 h-[1px] bg-black"></div>
+      <h1 className="text-5xl font-cinzel font-bold">
+        MERGULHANDO NA FANTASIA
+      </h1>
+      <div className="w-2/4 h-[1px] bg-black"></div>
     </div>
   )
 }
