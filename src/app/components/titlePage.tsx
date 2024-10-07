45dd4f7d82fc46d2f24aa00f71d967bc06b1f212
viicsousa <victoria.vasconcelos@hotmail.com>
2024-10-07 11:15:21 -0300

    feat: title fininsh

--- src/app/components/titlePage.tsx
+++ src/app/components/titlePage.tsx
@@ -2,9 +2,10 @@ export default function TitlePage () {
   return (
     <div className=" flex flex-col justify-center items-center gap-4 p-6">
       <div className="w-2/4 h-[1px] bg-black"></div>
-      <h1 className="text-5xl font-cinzel font-bold">
-        MERGULHANDO NA FANTASIA
-      </h1>
+      <h1 className="text-5xl font-cinzel font-bold tracking-wider text-[#003366] text-outline mx-auto text-center py-4" style={{ maxWidth: '600px' }}>
+  Mergulhando na fantasia
+</h1>
+
       <div className="w-2/4 h-[1px] bg-black"></div>
     </div>
   )
