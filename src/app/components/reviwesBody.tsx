d52cff15523d1c73f19485082b0c1731c116395f
viicsousa <victoria.vasconcelos@hotmail.com>
2024-10-12 10:23:53 -0300

    feat:

--- src/app/components/reviwesBody.tsx
+++ src/app/components/reviwesBody.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import CoverBook from "../../assets/svg/book.svg";
+import BackBook from "../../assets/jpeg/bookback.jpg";
 import Twitch from "../../assets/png/twitch.png";
 import Linkedin from "../../assets/png/linkedin.png";
 import Instagram from "../../assets/png/instagram.png";
@@ -17,23 +18,27 @@ export default function ReviewsBody () {
     <div className="flex justify-center">
   <div className="grid grid-cols-[80%_20%] gap-4 py-4 w-3/4">
     {/* Coluna 1: Livro */}
-    <div className="relative bg-white p-4 rounded-2xl ">
+    <div className="relative bg-white p-4 rounded-2xl">
   {/* Imagem de fundo com opacidade */}
   <div
-    className="absolute inset-0 bg-cover opacity-10 w-full"
-    style={{ backgroundImage: `url(${CoverBook.src})` }} // Usando a propriedade src
+    className="absolute inset-0 bg-cover opacity-15 rounded-2xl "
+    style={{ backgroundImage: `url(${BackBook.src})` }} // Usando a propriedade src
   ></div>
 
   {/* Conteúdo */}
-  <div className="relative z-10">
-    <Image src={CoverBook} alt="Coverbook" />
-    <div>
-      <h3>Lovely War</h3>
-      <p>Autora: Júlia Berry</p>
-      <p>Data da publicação: 5 de março de 2019</p>
-      <p>Páginas: 480</p>
+  <div className="grid relative z-10 p-2">
+   
+    <div className="flex">
+    <Image src={CoverBook} alt="Coverbook" className="w-60" />
+      <div className="px-4">
+      <h3 className="text-2xl font-cinzel font-bold text-[#003366] py-4">Lovely War</h3>
+      <p className="text-xl py-1"><b>Autora:</b> Júlia Berry</p>
+      <p className="text-xl py-1"><b>Data da publicação:</b> 5 de março de 2019</p>
+      <p className="text-xl py-1"><b>Páginas:</b> 480</p>
+      </div>
     </div>
-    <div>Resenha</div>
+    <p className="text-xl text-justify p-1"></p>
+    
   </div>
 </div>
 
@@ -45,7 +50,7 @@ export default function ReviewsBody () {
       {/* Sobre mim */}
       <div className="bg-white p-4 rounded-2xl">
         <h2 className="text-center text-2xl font-cinzel font-bold text-[#003366] py-4">Victória</h2>
-        <p className="text-justify">
+        <p className="text-justify ">
           Criei este site para aprender mais sobre programação, voltado para algo que amo: livros.
           Aqui, vou compartilhar minhas leituras favoritas e algumas resenhas que escrevi ao longo dos
           anos...
